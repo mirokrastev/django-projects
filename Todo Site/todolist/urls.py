dd6f1d05ff94a8f6bf7068790de558a41b17b2ce
mirokrastev <mirokrastev@protonmail.com>
2020-08-19 23:38:51 +0300

    Small method update
    Small HTML method updates.

--- Todo Site/todolist/urls.py
+++ Todo Site/todolist/urls.py
@@ -4,8 +4,8 @@ from todolist.views import detailed_todo_view, complete_todo_view, \
 
 urlpatterns = [
     path('<int:task_pk>/', detailed_todo_view, name='detailed_todo'),
-    path('<int:task_pk>/complete/', complete_todo_view, name='complete_todo'),
-    path('<int:task_pk>/delete/', delete_todo_view, name='delete_todo'),
+    path('<int:task_pk>/complete', complete_todo_view, name='complete_todo'),
+    path('<int:task_pk>/delete', delete_todo_view, name='delete_todo'),
     path('create/', create_todo_view, name='create'),
     path('completed/', completed_todos_view, name='completed_todos'),
 ]
